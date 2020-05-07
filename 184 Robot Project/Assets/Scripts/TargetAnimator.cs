662b9cfaf78ec747df76071b8f8cd6df2e807602
eric-mccormick <eric.mccormick@berkeley.edu>
2020-05-06 21:37:20 -0700

    Target Animator built

--- 184 Robot Project/Assets/Scripts/TargetAnimator.cs
+++ 184 Robot Project/Assets/Scripts/TargetAnimator.cs
@@ -2,17 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TargetAnimator : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class TargetAnimator : MonoBehaviour {
+    #region Editor Variables
+    [SerializeField]
+    [Tooltip("The animator controller for the right hand target")]
+    private Animator m_RightHand;
+
+    [SerializeField]
+    [Tooltip("The animator controller for the right foot target")]
+    private Animator m_RightFoot;
+
+    [SerializeField]
+    [Tooltip("The animator controller for the left hand target")]
+    private Animator m_LeftHand;
+
+    [SerializeField]
+    [Tooltip("The animator controller for the left foot target")]
+    private Animator m_LeftFoot;
+    #endregion
+
+    #region Private Variables
+
+    #endregion
+
+    private void Awake() {
+        m_RightHand.SetBool("Idle", true);
+        m_RightFoot.SetBool("Idle", true);
+        m_LeftHand.SetBool("Idle", true);
+        m_LeftHand.SetBool("Idle", true);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.R)) {
+            Reset();
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            Wave();
+        }
     }
+
+    #region Animations
+    private void Reset() {
+        m_RightHand.SetBool("Idle", true);
+        m_RightFoot.SetBool("Idle", true);
+        m_LeftHand.SetBool("Idle", true);
+        m_LeftHand.SetBool("Idle", true);
+    }
+
+    private void Wave() {
+        m_RightHand.SetBool("Wave", true);
+        m_RightHand.SetBool("Idle", false);
+    }
+    #endregion
 }
